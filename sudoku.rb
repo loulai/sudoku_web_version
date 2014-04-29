103668f6460dd5439dcd54453299e2b73a005b35
DLDB <dpbergquist@gmail.com>
2014-04-29 18:07:13 +0100

    solved a sudoku (back end only)

--- sudoku.rb
+++ sudoku.rb
@@ -1,4 +1,13 @@
 require 'sinatra' # load sinatra
+require_relative './lib/sudoku'
+require_relative './lib/cell'
+
+def random_sudoku
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Sudoku.new(seed.join)
+  sudoku.solve!
+  sudoku.to_s.chars
+end
 
 get '/' do # default route for our website
   erb :index
